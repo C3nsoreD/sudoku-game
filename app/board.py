18482bb4469f418dc4e1028713068d9a7639244d
Gabriel R <37087631+C3nsoreD@users.noreply.github.com>
2020-05-19 12:05:56 +0200

    Update board.py

--- app/board.py
+++ app/board.py
@@ -6,33 +6,33 @@ BOARD_SIZE = 9
 """ Randomly generates sudoku game boards
 """
 
-# class BoardGameGenerator:
-#     def __init__(self):
-#         pass
+class BoardGameGenerator:
+    def __init__(self):
+        pass
 
-#     def __create_board(self):
-#         '''
-#         This helper function checks and constructs the 2D board 
-#         '''
-#         board = [] 
+    def __create_board(self):
+        '''
+        This helper function checks and constructs the 2D array that will contain the board game
+        '''
+        board = [] 
         
-#         # Each line must satisfy game conditions. i.e only 9 numbers, doesnot check if the board is valid
-#         for line in boardfile:
-#             line = line.strip() # create a list of the elements
-#             if len(line) != BOARD_SIZE:
-#                 raise GameError("There must be 0-9 numbers in a row")
+        # Each line must satisfy game conditions. i.e only 9 numbers, doesnot check if the board is valid
+        for line in boardfile:
+            line = line.strip() # create a list of the elements
+            if len(line) != BOARD_SIZE:
+                raise GameError("There must be 0-9 numbers in a row")
             
-#             board.append([]) # Create an empty 2D board 
+            board.append([]) # Create an empty 2D board 
             
-#             # Checks charaters
-#             for c in line:
-#                 if not c.isdigit():
-#                     raise GameError("Only numbers 1-9 allowed")
+            # Checks charaters
+            for c in line:
+                if not c.isdigit():
+                    raise GameError("Only numbers 1-9 allowed")
                 
-#                 board[-1].append(int(c))
+                board[-1].append(int(c))
 
-#         if len(board) != 9:
-#             raise GameError("The board has to have 9 rows")
-#         return board
-    
+        if len(board) != 9:
+            raise GameError("The board has to have 9 rows")
+        return board
 
+    # Add generator functiono 
